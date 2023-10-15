5b543ab417c571ae4e5f63f4b50a55471d30b659
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-10-14 23:49:09 -0400

    build: initial
    built basic start menu and play window, began working on functionality, barely. The journey begins

---
+++ assets/js/game.js
@@ -0,0 +1,30 @@
+//Window variables
+const startButton = document.getElementById('startButton');
+const startWindow = document.getElementById('startWindow');
+const gameWindow = document.getElementById('gameWindow')
+
+//Main game function
+function startGame() {
+    startWindow.style.visibility = "hidden";
+    gameWindow.style.visibility = 'visible';
+    console.log("Game Start");
+};
+
+startButton.addEventListener("click", startGame);
+
+//Player class
+
+//level class and array
+
+//enemy class and array
+
+//loot, loot array and loot tables
+
+//combat functions
+
+//testing area
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+const test = document.getElementById('testButton');
