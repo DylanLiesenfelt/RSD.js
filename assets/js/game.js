557b6a83d76f99694321417f3e45ccaded437243
Dylan Liesenfelt <120532193+DylanLiesenfelt@users.noreply.github.com>
2023-10-19 20:38:46 -0400

    build: game clock
    built game clock, does not iterate at second pace, need to fix

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,11 +3,25 @@ const startButton = document.getElementById('startButton');
 const startWindow = document.getElementById('startWindow');
 const gameWindow = document.getElementById('gameWindow')
 
+//Game Clock
+function gameClock (){
+    for (let hours = 0; hours < 24; hours++) {
+        for (let minuets = 0; minuets < 60; minuets++) {
+            for (let seconds = 0; seconds < 60; seconds++) {
+                setTimeout(() => console.log(hours,':',minuets,':',seconds), 1000)
+            }
+        }
+    }
+}
+
+
 //Main game function
 function startGame() {
+    //Start up processes
     startWindow.style.visibility = "hidden";
     gameWindow.style.visibility = 'visible';
     console.log("Game Start");
+    gameClock();
 };
 
 startButton.addEventListener("click", startGame);
