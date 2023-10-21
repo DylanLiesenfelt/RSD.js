8e017f97562c96d2c13f77d48c1789e3acaf1176
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-10-21 01:55:18 -0400

    build: classes and variables
    made enemy class and some enemy objects, assigned i think all window elements, made room class constructor, and set an outline of what needs to be done

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,7 +4,7 @@
 
 /*----------  Window Variables ----------*/
 const startWindow = document.getElementById('startWindow');
-const gameWindow = document.getElementById('gameWindow')
+const gameWindow = document.getElementById('gameWindow');
 /*----------  Element Variables  ----------*/
 const startButton = document.getElementById('startButton');
 /* Player Display Elements */
@@ -20,35 +20,86 @@ const smithingLevel = document.getElementById('smithingLevel');
 const fishingLevel = document.getElementById('fishingLevel');
 const playerLevel = document.getElementById('playerLevel');
 const expBar = document.getElementById('expBar');
-const currentExp = document.getElementById('currentExp')
-const nextLevelExp = document.getElementById('nextLevelExp')
+const currentExp = document.getElementById('currentExp');
+const nextLevelExp = document.getElementById('nextLevelExp');
 const inventoryWeight = document.getElementById('inventoryWeight');
-const currentWeight = document.getElementById('currentWeight')
-const maxWeight = document.getElementById('maxWeight')
+const currentWeight = document.getElementById('currentWeight');
+const maxWeight = document.getElementById('maxWeight');
 /* Story Display Elements */
-const gameClockDisplay = document.getElementById('gameClockDisplay')
+const roomName = document.getElementById('roomName');
+const roomCounter = document.getElementById('roomCounter');
+const imageDisplay = document.getElementById('imageDisplay');
+const gameClockDisplay = document.getElementById('gameClockDisplay');
+
 
 /*=============================================
-=                  Functions                  =
+=                    Rooms                    =
+=============================================*/
+class Room {
+    constructor(name, level, image, message) {
+        this.name = name;
+        this.level = level;
+        this.image = image;
+        this.message = message;
+    };
+};
+const roomList = [];
+
+
+/*=============================================
+=                   Enemies                   =
+=============================================*/
+class Enemy {
+    constructor(name, level, health, attack, defense, dexterity, image) {
+        this.name = name;
+        this.level = level;
+        this.health = health;
+        this.attack = attack;
+        this.defense = defense;
+        this.dexterity = dexterity;
+        this.image = image;
+    };
+};
+
+const rat = new Enemy('Rat', 1, 5, 1, 1, 1, '');
+const slime = new Enemy('Slime', 2, 10, 1, 2, 1, ''); 
+const gobbler = new Enemy('Gobbler', 3, 10, 3, 3, 2, ''); //goblin. but more gobble, maybe that thing turkeys got on thier throats
+const basklisk = new Enemy('Basklisk', 5, 20, 5, 10, 5, ''); //beefy lizard
+const gardenHermit = new Enemy('Garden Hermit', 5, 15, 3, 3, 10, ''); // old man wearing a trash bag 
+const lizardMan = new Enemy('Lizard Man', 10, 30, 12, 10, 10, ''); // Mitch McConnel
+const jackedRabbit = new Enemy('Jacked Rabbit', 12, 35, 20, 5, 12, '') // play on jack rabbit, also this image https://ehkern.files.wordpress.com/2016/07/e28098the-smithfield-decretals_-decretals-of-gregory-ix-with-glossa-ordinaria-tolouse-ca-1300-illuminations-added-in-london-ca-1340-british-library-royal-10-e-iv-fol-61v.jpg?w=300&h=290
+const sentinel = new Enemy('Sentinel', 15, 30, 15, 15, 5, '') // A Statue with black eyeholes, like those greek ones
+const ashman = new Enemy('Ash Man', 20, 40, 20, 10 , 15, ''); //zombie smoke ash kinda guy
+const hydra = new Enemy();
+const banshee = new Enemy();
+const cavewhale = new Enemy(); //a small whale made out of rocks and stuff
+const bat = new Enemy();
+const blackShuck = new Enemy();
+const gjenganger = new Enemy();
+const wako = new Enemy();
+const babayaga = new Enemy();
+const jinn = new Enemy();
+const tejujagua = new Enemy();
+const anansi = new Enemy();
+const wani = new Enemy();
+const huldfolk = new Enemy();
+const troll = new Enemy();
+const krampus = new Enemy();
+const yetti = new Enemy();
+const windogo = new Enemy();
+const skunkape = new Enemy();
+
+
+const enemyList = [rat, slime];
+
+
+/*=============================================
+=                   Items                    =
 =============================================*/
 
-/*----------  Game Clock  ----------*/
-//Function to delay itterations
-function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
-}
-//Function to count time 
-async function gameClock() {
-    for (let hours = 0; hours < 24; hours++) {
-        for (let minutes = 0; minutes < 60; minutes++) {
-            for (let seconds = 0; seconds < 60; seconds++) {
-                const timeString = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
-                gameClockDisplay.querySelector('#currentTime').textContent = timeString;
-                await sleep(1000);
-            }
-        }
-    }
-}
+
+
+
 
 /*=============================================
 =                   Player                    =
@@ -83,9 +134,46 @@ let denominatorExp = 100;
 nextLevelExp.textContent = `/ ${denominatorExp} `;
 /* Inventory */
 let heldWeight = 0;
-currentWeight.textContent = `${heldWeight}`
+currentWeight.textContent = `${heldWeight}`;
 let overcumber = 100;
-maxWeight.textContent = `${overcumber} LBS`
+maxWeight.textContent = `${overcumber} LBS`;
+
+/*=============================================
+=                  Functions                  =
+=============================================*/
+
+/*----------  Game Clock  ----------*/
+//Function to delay itterations
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+//Function to count time 
+async function gameClock() {
+    for (let hours = 0; hours < 24; hours++) {
+        for (let minutes = 0; minutes < 60; minutes++) {
+            for (let seconds = 0; seconds < 60; seconds++) {
+                const timeString = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+                gameClockDisplay.querySelector('#currentTime').textContent = timeString;
+                await sleep(1000);
+            };
+        };
+    };
+};
+
+
+/*----------  Load New Room  ----------*/
+
+
+/*----------  Load Enemy  ----------*/
+
+
+/*----------  Combat  ----------*/
+
+
+/*----------  Message update  ----------*/
+
+
+
 
 /*=============================================
 =                Game Runtime                =
