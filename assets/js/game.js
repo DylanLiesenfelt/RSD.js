99473a0aa59b1e71eddfe87b95898a4033259d7b
DylanLiesenfelt <dylanjames.code@gmail.com>
2023-10-27 01:00:20 -0400

    update: restructuring of js
    writing functions into different files and calling them into main game function separately, more to do

--- assets/js/game.js
+++ assets/js/game.js
@@ -154,6 +154,7 @@ maxWeight.textContent = `${overcumber} LBS`;
 =                  Functions                  =
 =============================================*/
 
+
 /*----------  Game Clock  ----------*/
 //Function to delay itterations
 function sleep(ms) {
@@ -176,103 +177,15 @@ async function gameClock() {
 /*----------  Load New Room  ----------*/
 
 
-/*----------  Load Enemy  ----------*/
-function pullEnemy() {
-    let randomEnemy = Math.floor(Math.random() * enemyList.length);
-    let selectedEnemy = enemyList[randomEnemy];
-
-    while (selectedEnemy.level > level + 2 || selectedEnemy.level < level - 5) {
-        randomEnemy = Math.floor(Math.random() * enemyList.length);
-        selectedEnemy = enemyList[randomEnemy];
-    };
-
-    return selectedEnemy;
-};
-
-let enemy = pullEnemy(enemyList, level);
-
-function loadEnemy(enemy) {
-    textDisplay.textContent = `You are being attacked by a level ${enemy.level} ${enemy.name}`
-    enemyDisplay.style.backgroundImage = `url(${enemy.image})`;
-    enemyInfo.textContent = `HP: ${enemy.health}`;
-    
-}
-
-/*----------  Combat  ----------*/
-   //Combat function for the player
-    function playerMeleeCombat(attack, dexterity, luck, enemy) {
-
-        let playerHitChance = dexterity * ((Math.floor(Math.random() * 11)) + luck); //Rolls player value for hit calc
-        let enemyEvadeChance = enemy.dexterity * Math.floor(Math.random() * 11); // Rolls enemy value for hit calc
-        let playerMaxDamage = attack * ((Math.floor(Math.random() * 11)) + luck); //Rolls max damage value for what player can hit
-        let playerCritChance = luck * Math.floor(Math.random() * 11);
-        let enemyDamageTaken = playerMaxDamage - (enemy.defense * (Math.floor(Math.random() * 3)));
-
-        if (enemyEvadeChance > playerHitChance) {
-            console.log("Player Misses");
-            textDisplay.textContent = `You missed. -0 HP Damage to ${enemy.name}`;
-            console.log('Test', `Hit Chance: ${playerHitChance}`, `Enemy Evade: ${enemyEvadeChance}`, `Max Damage: ${playerMaxDamage}`, `Crit Chance: ${playerCritChance}`, `Damage Reg: ${enemyDamageTaken}`)
-            console.log(`Test`, `ATK: ${atk},`, `DEX: ${dex}`, `LUCK: ${luck}`)
-            console.log(`Test enemy dex:${enemy.dexterity}`)
-        } else {
-            if (playerCritChance > enemyEvadeChance) {
-                console.log('Player Crits')
-                enemy.health -= playerMaxDamage
-                textDisplay.textContent = `You Got A Critical Hit! -${playerMaxDamage} HP to ${enemy.name}`
-                console.log(`Damage: ${playerMaxDamage}`, `Enemy HP: ${enemy.health}`)
-                console.log('Test', `Hit Chance: ${playerHitChance}`, `Enemy Evade: ${enemyEvadeChance}`, `Max Damage: ${playerMaxDamage}`, `Crit Chance: ${playerCritChance}`, `Damage Reg: ${enemyDamageTaken}`)
-                console.log(`Test`, `ATK: ${atk},`, `DEX: ${dex}`, `LUCK: ${luck}`)
-                console.log(`Test enemy dex:${enemy.dexterity}`)
-            } else {
-                console.log('Player Hits')
-                enemy.health -= enemyDamageTaken
-                textDisplay.textContent = `You hit. -${enemyDamageTaken} HP to ${enemy.name}`
-                console.log(`Damage: ${enemyDamageTaken}`, `Enemy HP: ${enemy.health}`)
-                console.log('Test', `Hit Chance: ${playerHitChance}`, `Enemy Evade: ${enemyEvadeChance}`, `Max Damage: ${playerMaxDamage}`, `Crit Chance: ${playerCritChance}`, `Damage Reg: ${enemyDamageTaken}`)
-                console.log(`Test`, `ATK: ${atk},`, `DEX: ${dex}`, `LUCK: ${luck}`)
-                console.log(`Test enemy dex:${enemy.dexterity}`)
-            }
-            return enemy.health, enemyInfo.textContent = `HP: ${enemy.health}`;
-        }
-    };
 
  
 /*=============================================
 =                Game Runtime                =
 =============================================*/
 function game() {
-    startWindow.style.visibility = "hidden";
-    gameWindow.style.visibility = 'visible';
-
-    //Game Starts
-    console.log("Game Start");
-    timeElapsed = gameClock();
-
-    //Sets Up Entrance
-    imageDisplay.style.backgroundImage = `url(${entrance.image})`;
-    roomCounterValue += 1;
-    roomCounter.textContent = roomCounterValue;
-    roomName.textContent = entrance.name;
-    textDisplay.textContent = entrance.message;
-    choice1Button.style.visibility = 'visible';
-    choice1Button.textContent = 'YES';
+    startGame()
     
-    choice1Button.addEventListener("click", function() {
-        choice1Button.style.visibility = 'hidden'
-        loadEnemy(enemy)
-        enemyInfo.style.visibility = 'visible'
-        meleeButton.style.visibility = 'visible'
-    })
-    
-    // add a check for health value of player and enemy to determine if combat is over
-    meleeButton.addEventListener('click', function() {
-        playerMeleeCombat(atk, dex, luck, enemy)
-        //add enemy combat function
-    })
-    
-    //add a check if combat is over give new choice for new room load. if left odd number lengths if right even number lengths of room index
-    
-
+     
 };
 
 startButton.addEventListener("click", game);
--- assets/js/start.js
+++ assets/js/start.js
@@ -3,12 +3,43 @@ function startMenu(start, game) {
     game.style.visibility = 'visible'
 };
 
-function loadEntrance(imageDisplay, roomCounterValue, roomCounter, roomName, textDisplay, choice1Button) {
-    imageDisplay.style.backgroundImage = `url(${entrance.image})`;
-    roomCounterValue += 1;
-    roomCounter.textContent = roomCounterValue;
-    roomName.textContent = entrance.name;
-    textDisplay.textContent = entrance.message;
-    choice1Button.style.visibility = 'visible';
-    choice1Button.textContent = 'YES';
+/*----------  Game Clock  ----------*/
+//Function to delay itterations
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+//Function to count time 
+async function gameClock() {
+    for (let hours = 0; hours < 24; hours++) {
+        for (let minutes = 0; minutes < 60; minutes++) {
+            for (let seconds = 0; seconds < 60; seconds++) {
+                const timeString = `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(seconds).padStart(2, '0')}`;
+                gameClockDisplay.querySelector('#currentTime').textContent = timeString;
+                await sleep(1000);
+            };
+        };
+    };
 };
+
+
+function startGame() {
+    startMenu(startWindow, gameWindow, choice1Button);
+    console.log("Game Start");
+    timeElapsed = gameClock();
+
+    choice1Button.style.visibility = 'visible'
+    choice1Button.textContent = 'YES';
+
+    imageDisplay.style.backgroundImage = `url(${entrance.image})`;
+    textDisplay.textContent = entrance.message;
+    roomName.textContent = entrance.name;
+    
+    choice1Button.addEventListener("click", function() {
+        choice1Button.style.visibility = 'hidden'
+        roomCounterValue += 1;
+        roomCounter.textContent = roomCounterValue;
+    })
+    // First combat
+    combat()
+}
+    
